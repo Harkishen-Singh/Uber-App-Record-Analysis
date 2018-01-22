674ee0b0b2e080ae3e9cb06fb2cda1252b109624
Harkishen Singh <harkishensingh@hotmail.com>
2018-01-22 16:17:29 +0530

    added features

--- creating database.py
+++ creating database.py
@@ -41,21 +41,74 @@ class Data_extraction_creation:
         self.search="milli display--block" # first day
         search_length=len(self.search)
         c=0
+        trip_number=0
+        day=""
         for i in range(0, len((self.file))-search_length): # counting individual trip of that day.
             substr = self.file[i:i+search_length]
+
             if self.search == substr:
-                c = c + 1
-                day = self.file[i+search_length+2:i+search_length+12]
+                c = c + 1 # day count
+                day = self.file[i+search_length+2:i+search_length+13]
                 search2="trip-table__row"
                 search_length2=len(search2)
+
                 m=0
                 for j in range(0, len((self.file))-search_length2):
+
                     substr2 = self.file[j:j+search_length2]
                     if substr2 == search2:
+                        trip_number = trip_number+1
                         m=m+1
-                        if m==1:
-                            trip_start=self.file[j+search_length2+7: j+search_length2+ 14]
-                            trip_time = self.file[j+search_length2+38: j+search_length2+45]
+                        count=0
+                        for k in range(1, 1000): #<td> and </td> count search
+                            s1 = "<td>"
+                            s2 = "</td>"
+                            pos_of_s2=0
+                            s3 = self.file[j+search_length2 +k: j+search_length2 +k+4]
+                            s4 = self.file[j+search_length2 +k: j+search_length2 +k+5]
+
+                            if s3 == s1:
+                                count = count +1
+                                if count==1:
+                                    for pp in range(0,20):
+                                        sear= self.file[j+search_length2 +k+pp: j+search_length2 +k+pp+5]
+                                        if sear == s2:
+                                            pos_of_s2=pp
+                                    trip_start=str(self.file[j+search_length2 +k+4: j+search_length2 +k+pos_of_s2])
+                                if count==3:
+                                    for pp in range(0,20):
+                                        sear= self.file[j+search_length2 +k+pp: j+search_length2 +k+pp+5]
+                                        if sear == s2:
+                                            pos_of_s2=pp
+                                    trip_time=str(self.file[j+search_length2 +k+4: j+search_length2 +k+pos_of_s2])
+
+                                if count==4:
+                                    for pp in range(0,20):
+                                        sear= self.file[j+search_length2 +k+pp: j+search_length2 +k+pp+5]
+                                        if sear == s2:
+                                            pos_of_s2=pp
+                                    trip_km=str(self.file[j+search_length2 +k+4: j+search_length2 +k+pos_of_s2])
+
+                                if count==5:
+                                    for pp in range(0,20):
+                                        sear= self.file[j+search_length2 +k+pp: j+search_length2 +k+pp+5]
+                                        if sear == s2:
+                                            pos_of_s2=pp
+                                    trip_cash_collected=str(self.file[j+search_length2 +k+4: j+search_length2 +k+pos_of_s2])
+
+                                if count==6:
+                                    break
+                        print("day = "+ day)
+                        print("trip_number"+ str(trip_number))
+                        print("trip_km "+ trip_km)
+                        print("trip_time = "+ trip_time)
+                        print("trip_start = "+ trip_start)
+                        print("trip_cash_collected = "+ trip_cash_collected)
+                        print("\n\n")
+
+
+
+
 
 
 
